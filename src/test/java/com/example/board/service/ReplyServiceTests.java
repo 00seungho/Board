52ddf03c0043e1885a77cd7d7dde497b54a6def6
00seungho <tmdgh9910@naver.com>
2024-10-29 16:30:06 +0900

    reply JSON 구현

---
+++ src/test/java/com/example/board/service/ReplyServiceTests.java
@@ -0,0 +1,24 @@
+package com.example.board.service;
+
+
+import com.example.board.dto.ReplyDTO;
+import com.example.board.entity.Reply;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class ReplyServiceTests {
+    @Autowired
+    private ReplyService replyService;
+
+    @Test
+    public void testGetList() {
+        Long bno = 97L;
+        List<ReplyDTO> replyDTOList = replyService.getList(bno);
+        replyDTOList.forEach(replyDTO -> System.out.println(replyDTO));
+
+    }
+}
